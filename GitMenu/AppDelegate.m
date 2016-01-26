570cd450fb1f3da38542d35e13b0dd9dbae78027
Morgan Lieberthal <morgan@jmorgan.org>
2016-01-26 16:04:08 -0700

    Refactor AppDelegate and Add Lumberjack setup

--- GitMenu/AppDelegate.m
+++ GitMenu/AppDelegate.m
@@ -7,20 +7,55 @@
 //
 
 #import "AppDelegate.h"
+#import "JMLLoggerColors.h"
 
 @interface AppDelegate ()
 
-@property (weak) IBOutlet NSWindow *window;
+@property(weak) IBOutlet NSWindow *window;
+@property(weak) IBOutlet NSTextField *myNumberField;
+@property(strong, readonly) NSUserDefaults *sharedUserDefaults;
+
 @end
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+@synthesize sharedUserDefaults = _sharedUserDefaults;
+
++ (void)initialize
+{
+    [super initialize];
+    JML_InitializeCocoaLumberjack();
+    JML_InitializeXCodeConsoleColors();
 }
 
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
+- (NSUserDefaults *)sharedUserDefaults
+{
+    if (_sharedUserDefaults) {
+        return _sharedUserDefaults;
+    }
+
+    _sharedUserDefaults = [[NSUserDefaults alloc] initWithSuiteName:GMUAppGroupIdentifier];
+
+    return _sharedUserDefaults;
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    NSInteger myNumber = [self.sharedUserDefaults integerForKey:@"MyNumberKey"];
+    if (myNumber) {
+        [self.myNumberField setIntegerValue:myNumber];
+    }
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification
+{
     // Insert code here to tear down your application
+}
+
+- (IBAction)saveButtonHit:(id)sender
+{
+    [self.sharedUserDefaults setInteger:[self.myNumberField integerValue] forKey:@"MyNumberKey"];
+    [self.sharedUserDefaults synchronize];
 }
 
 @end
